fa0fb20c43a8a9f1edd7297fad20d6b99846ce72
mahmoud ramadan <95543177+mhmoudramadan@users.noreply.github.com>
2022-04-07 06:39:10 +0200

    Add files via upload

---
+++ HCAN_Config.h
@@ -0,0 +1,16 @@
+/*
+ * HCAN_Config.h
+ *
+ *  Created on: Mar 22, 2022
+ *      Author: Dell
+ */
+
+#ifndef HAL_HCAN_HCAN_CONFIG_H_
+#define HAL_HCAN_HCAN_CONFIG_H_
+
+/* Run at 250KHz Bit Rate Using 16MHz OSC */
+#define CNF1_VALUE		0x80
+#define CNF2_VALUE		0xB1
+#define CNF3_VALUE		0x05
+
+#endif /* HAL_HCAN_HCAN_CONFIG_H_ */
